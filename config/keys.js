01597c34b1565110d67d5a2d37eaf7cb0e99c03a
Unknown <khmike@hotmail.ca>
2018-12-10 16:08:24 -0500

    Added routes
    Added routes and .gitignore file

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://khmike1:khmike1@ds143039.mlab.com:43039/dev-in"
+};
--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
+//db config
+const db = require("./config/keys.js").mongoURI;
+//db connection
+mongoose
+  .connect(db)
+  .then(() =>
+    console.log(
+      "\n\nSUCCESS:\n***************\nMongoDB connected" +
+        "\n***************\n\n"
+    )
+  )
+  .catch(err =>
+    console.log(
+      "\n\nERROR ENCOUNTERED:\n***************\n" +
+        err +
+        "\n***************\n\n"
+    )
+  );
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+//use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(
+    `\n\nSUCCESS:\n***************\nServer running on port ${port} \n***************\n\n`
+  )
+);
