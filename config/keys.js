b30ecee49b26dacf30896dcda036f1480a5f457d
Unknown <khmike@hotmail.ca>
2018-12-11 15:20:24 -0500

    JWT token + Passport + Validation using Postman
    Added passport hashing, retrieval of JWT token and user validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://khmike1:khmike1@ds143039.mlab.com:43039/dev-in"
+  mongoURI: "mongodb://khmike1:khmike1@ds143039.mlab.com:43039/dev-in",
+  secretOrKey: "secret"
 };
