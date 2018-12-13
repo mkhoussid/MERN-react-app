b564f0cd6d90f241928ae9d19d46ddfda38f6031
Unknown <khmike@hotmail.ca>
2018-12-13 16:52:21 -0500

    Added basic layout
    Footer, Navbar and Landing added

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <div className="landing">
+          <div className="dark-overlay landing-inner text-light">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <h1 className="display-3 mb-4">
+                    Dev<span className="lt">&lt;</span>-n
+                  </h1>
+                  <p className="lead">
+                    {" "}
+                    Dive in with Dev<span className="lt">&lt;</span>-n
+                  </p>
+                  <hr />
+                  <a href="register.html" className="btn btn-lg btn-info mr-2">
+                    Sign Up
+                  </a>
+                  <a href="login.html" className="btn btn-lg btn-light">
+                    Login
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
