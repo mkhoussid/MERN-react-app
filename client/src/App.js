3c4cab5934a1d206d7dee576a0a7c252e36ad177
Unknown <khmike@hotmail.ca>
2018-12-14 16:09:41 -0500

    Added logging in and token transference between views/pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 //meeting place for front end
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -12,6 +16,16 @@ import Login from "./components/auth/Login";
 //import Blink from "./components/scripts/Blink";
 
 import "./App.css";
+
+//Check for token
+if (localStorage.jwtToken) {
+  //set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode it
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
