e2d1ce4d106e1e12509fd229c0b3e02da72d148d
Unknown <khmike@hotmail.ca>
2018-12-14 15:13:10 -0500

    Added token decoding

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,9 @@
 //register user action
-import { GET_ERRORS } from "./types";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 export const registerUser = (userData, history) => dispatch => {
   //axios to test backend api routes
@@ -15,4 +18,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//login / get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //save to local storage
+      const { token } = res.data; //destructure token
+      //set token to local storage
+      localStorage.setItem("jwtToken", token); //setItem only takes strings-use stringify
+      //set token to Auth header
+      setAuthToken(token);
+      //decode token to get user data
+      const decoded = jwt_decode(token);
+      //set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
