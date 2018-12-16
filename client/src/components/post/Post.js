51678e0224b758eb4cf04aa8923a9af8dd860f76
Unknown <khmike@hotmail.ca>
2018-12-16 15:32:07 -0500

    Show post item
    Recycled PostItem.js, added conditional display depending on showActions

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import PostItem from "../posts/PostItem";
 import Spinner from "../common/Spinner";
 import { getPost } from "../../actions/postActions";
 
@@ -11,9 +13,32 @@ class Post extends Component {
     this.props.getPost(this.props.match.params.id);
   }
   render() {
+    //make sure to pull from post state
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    //if post is empty or null or if state is loading
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+        </div>
+      );
+    }
     return (
-      <div>
-        <h1>Post</h1>
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back to Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
