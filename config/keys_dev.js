cc855880bd180ad0ddae386a8fa4fb97b2bcf2cc
unknown <khmike@hotmail.ca>
2018-12-16 22:23:26 -0500

    untracked node_modules

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://admin:admin@cluster0-shard-00-00-zdy0x.mongodb.net:27017,cluster0-shard-00-01-zdy0x.mongodb.net:27017,cluster0-shard-00-02-zdy0x.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "secret"
+};
