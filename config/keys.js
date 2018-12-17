044cd65b5816e2b04889cdadda5dae02f6f7cbde
unknown <khmike@hotmail.ca>
2018-12-16 21:57:00 -0500

    Deployment take 2

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://khmike1:khmike1@ds143039.mlab.com:43039/dev-in",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
